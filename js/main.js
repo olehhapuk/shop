4625fe131249040574171dea91f31b9d5a25a5cc
olehhapuk <oleggapuk20@gmail.com>
2020-05-03 18:34:17 +0300

    feat: created checkout and product pages

--- js/main.js
+++ js/main.js
@@ -1,40 +1,74 @@
-new Glider(document.querySelector('.glider'), {
-  slidesToShow: 1,
-  itemWidth: 270,
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next'
-  },
-  draggable: false,
-  scrollLock: true,
-  scrollLockDelay: 100,
-  duration: 0.9,
-  responsive: [
-    {
-      breakpoint: 425,
-      settings: {
-        slidesToShow: 'auto'
-      }
-    }
-  ]
-});
+window.addEventListener('load', () => {
+  const glider = document.querySelector('.glider');
+  const sidebarToggle = document.querySelector('.sidebar__toggle');
+  const sidebarItem = document.querySelectorAll('.sidebar__list-item');
+  const accordion = document.querySelectorAll('.product__details-title-item');
 
-document.querySelector('.sidebar__toggle').addEventListener('click', e => {
-  const target = document.querySelector('.sidebar__list');
+  if (glider !== null) {
+    new Glider(glider, {
+      slidesToShow: 1,
+      itemWidth: 270,
+      arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+      },
+      draggable: false,
+      scrollLock: true,
+      scrollLockDelay: 100,
+      duration: 0.9,
+      responsive: [{
+        breakpoint: 425,
+        settings: {
+          slidesToShow: 'auto'
+        }
+      }]
+    });
+  }
 
-  target.classList.toggle('sidebar__list_open');
-});
-
-document.querySelectorAll('.sidebar__list-item').forEach(item => {
-  item.addEventListener('click', e => {
-    try {
-      const targetId = e.target.dataset.target;
-      const target = document.getElementById(targetId);
+  if (sidebarToggle !== null) {
+    sidebarToggle.addEventListener('click', e => {
+      const target = document.querySelector('.sidebar__list');
   
       target.classList.toggle('sidebar__list_open');
-      e.target.classList.toggle('sidebar__list-item_open');
-    } catch (error) {
-      console.log(error);
-    }
-  });
-})
+    });
+  }
+
+  if (sidebarItem !== null) {
+    sidebarItem.forEach(item => {
+      item.addEventListener('click', e => {
+        try {
+          const targetId = e.target.dataset.target;
+          const target = document.getElementById(targetId);
+  
+          target.classList.toggle('sidebar__list_open');
+          e.target.classList.toggle('sidebar__list-item_open');
+        } catch (error) {
+          return;
+        }
+      });
+    });
+  }
+
+  if (accordion.length > 0) {
+    accordion[0].classList.add('product__details-title-item_active');
+    document.getElementById(accordion[0].dataset.target).classList.add('product__details-text-item_active');
+
+    accordion.forEach(item => {
+      item.addEventListener('click', e => {
+        const target = e.target.dataset.target;
+
+        accordion.forEach(aItem => {
+          const target = aItem.dataset.target;
+
+          document.getElementById(target).classList.remove('product__details-text-item_active');
+
+          aItem.classList.remove('product__details-title-item_active');
+        });
+
+        item.classList.add('product__details-title-item_active');
+        document.getElementById(target).classList.add('product__details-text-item_active');
+      });
+    });
+  }
+
+});
