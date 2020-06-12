d044be990ecb0c6c26514d2d644e4a69bf935684
olehhapuk <oleggapuk20@gmail.com>
2020-06-12 20:25:33 +0300

    feat: some new fixes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 window.addEventListener('load', () => {
+  const navbar = document.querySelector('.nav');
   const sidebarToggle = document.querySelector('.sidebar__toggle');
   const sidebarItem = document.querySelectorAll('.sidebar__list-item');
   const accordion = document.querySelectorAll('.product__details-title-item');
   const deliveryCheckbox = document.querySelector('#deliveryCheckbox');
   const deliveryInputContainer = document.querySelector('.form__input-container_delivery');
   const deliveryPrice = document.querySelector('#deliveryPrice');
+
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      navbar.style.top = "0";
+    } else {
+      navbar.style.top = "-55px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
 
   if (sidebarToggle !== null) {
     sidebarToggle.addEventListener('click', e => {
