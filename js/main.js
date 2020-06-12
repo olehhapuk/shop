c3a4937905283bb772c204df4782566d49bde412
olehhapuk <oleggapuk20@gmail.com>
2020-06-12 20:33:33 +0300

    fix: navbar proper hide

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@ window.addEventListener('load', () => {
   window.onscroll = function() {
     const currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-      navbar.style.top = "0";
+      navbar.style.transform = 'translateY(0)';
     } else {
-      navbar.style.top = "-60px";
+      navbar.style.transform = 'translateY(-100%)';
     }
     prevScrollpos = currentScrollPos;
   }
